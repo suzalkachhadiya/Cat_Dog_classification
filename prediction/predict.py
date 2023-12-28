4c1e70aa46d98541d0a199db34fa31d5c69f2d12
suzal kachhadiya <suzalkachhadiya111@gmail.com>
2023-12-28 20:43:39 +0530

    third commit

--- prediction/predict.py
+++ prediction/predict.py
@@ -12,15 +12,15 @@ class dogCat:
 
         image_name=self.filename
         
-        test_image=image.load_img(image_name,target_size=(224,224))
+        test_image=image.load_img(image_name,target_size=(64,64))
         test_image=image.img_to_array(test_image)
         test_image=np.expand_dims(test_image,axis=0)
         result=np.argmax(model.predict(test_image),axis=1)
         print(result)
 
-        if result[0]==1:
-            prediction="Dog"
+        if result[0]!=1:
+            prediction="Cat"
             return [{"img":prediction}]
-        
-        prediction="Cat"
-        return [{"img":prediction}]
+        else:
+            prediction="dOG"
+            return [{"img":prediction}]
